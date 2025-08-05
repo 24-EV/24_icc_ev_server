d3211044fa800903cb75b7bc7bba3cb673e125df
nitepp04 <sondh1497@gmail.com>
2025-08-06 05:12:46 +0900

    feat : SERVER_MODE 에 맞게 소켓 dataReceived 설정

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,7 +1,7 @@
 const { parseData, getKoreaTime } = require("../utils/utils.js");
 const { saveToDynamoDB } = require("../services/dynamoDBService.js");
 const { createSocketServer } = require("../config/socketConfig.js");
-const { CONTROLLER_VERSION } = require("../config/envConfig.js");
+const { CONTROLLER_VERSION, SERVER_MODE } = require("../config/envConfig.js");
 const dataFormat = require("../constants/dataFormat.js");
 
 // 소켓 이벤트 핸들러
@@ -29,46 +29,48 @@ function initSocket(io) {
       }
     });
 
-    // 서버 기능 테스트용
-    setInterval(() => {
-      const dataWithKey = socketTester();
-
-      // 수신된 데이터를 클라이언트에 즉시 전송
-      socket.emit("dataReceived", dataWithKey);
-    }, 1000);
-
     // 연결 테스트
     socket.on("message", function (data) {
       console.log(getKoreaTime(), "받은 메시지 : ", data);
     });
 
-    // ESP32에서 데이터 수신 및 처리
-    socket.on("sendData", (receivedData) => {
-      try {
-        console.log("sendData 실행");
-        // receivedData가 28자리의 16진수 배열로 들어온다고 가정
-        // 이것도 이제 컨트롤러 버전마다 다름. 24년도 - 28자리, 28년도
-        const buffer = Buffer.from(receivedData, "hex"); // receivedData를 Buffer로 변환 (16진수)
-
-        // 데이터를 파싱하여 10진수로 변환
-        const parsedData = parseData(buffer);
-
-        // 파싱된 데이터 출력
-        console.log("Parsed Data:", parsedData);
-
-        // 데이터를 저장
-        saveToDynamoDB(parsedData);
+    if (SERVER_MODE !== "production") {
+      // 서버 기능 테스트용
+      setInterval(() => {
+        const dataWithKey = socketTester();
 
         // 수신된 데이터를 클라이언트에 즉시 전송
-        socket.emit("dataReceived", parsedData);
-      } catch (error) {
-        console.error("데이터 처리 중 오류 발생:", error);
-        socket.emit("error", {
-          message: "데이터 처리 중 오류 발생",
-          detail: error.message,
-        });
-      }
-    });
+        socket.emit("dataReceived", dataWithKey);
+      }, 1000);
+    } else {
+      // ESP32에서 데이터 수신 및 처리
+      socket.on("sendData", (receivedData) => {
+        try {
+          console.log("sendData 실행");
+          // receivedData가 28자리의 16진수 배열로 들어온다고 가정
+          // 이것도 이제 컨트롤러 버전마다 다름. 24년도 - 28자리, 28년도
+          const buffer = Buffer.from(receivedData, "hex"); // receivedData를 Buffer로 변환 (16진수)
+
+          // 데이터를 파싱하여 10진수로 변환
+          const parsedData = parseData(buffer);
+
+          // 파싱된 데이터 출력
+          console.log("Parsed Data:", parsedData);
+
+          // 데이터를 저장
+          saveToDynamoDB(parsedData);
+
+          // 수신된 데이터를 클라이언트에 즉시 전송
+          socket.emit("dataReceived", parsedData);
+        } catch (error) {
+          console.error("데이터 처리 중 오류 발생:", error);
+          socket.emit("error", {
+            message: "데이터 처리 중 오류 발생",
+            detail: error.message,
+          });
+        }
+      });
+    }
 
     socket.on("disconnect", (reason) => {
       console.log("사용자 연결 해제됨:", reason);
